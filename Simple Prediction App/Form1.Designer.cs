fac339221d4b901fca94571a1208e08c69256890
Maxim <generalmaxis@yandex.ru>
2021-05-24 20:56:48 +0500

    Добавьте файлы проекта.

---
+++ Simple Prediction App/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Simple_Prediction_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.bPredict = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.bPredict);
+            this.splitContainer1.Size = new System.Drawing.Size(345, 152);
+            this.splitContainer1.SplitterDistance = 70;
+            this.splitContainer1.SplitterIncrement = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(341, 66);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // bPredict
+            // 
+            this.bPredict.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bPredict.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bPredict.Font = new System.Drawing.Font("Blade Runner Movie Font", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.bPredict.Location = new System.Drawing.Point(0, 0);
+            this.bPredict.Name = "bPredict";
+            this.bPredict.Size = new System.Drawing.Size(341, 74);
+            this.bPredict.TabIndex = 0;
+            this.bPredict.Text = "PREDICT";
+            this.bPredict.UseVisualStyleBackColor = true;
+            this.bPredict.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 152);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Predictor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button bPredict;
+    }
+}
+
---
+++ Simple Prediction App/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace Simple_Prediction_App
+{
+    public partial class Form1 : Form
+    {
+        private const string appName = "Simple Predictor";
+        private readonly string predictionsFile = $"{Environment.CurrentDirectory}\\predictionConfig.json";
+        private string[] predictions;
+        private static Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            splitContainer1.IsSplitterFixed = true;
+            this.Text = appName;
+
+            try
+            {
+                var data = File.ReadAllText(predictionsFile,Encoding.UTF8);
+                predictions = JsonConvert.DeserializeObject<string[]>(data);
+                MessageBox.Show($"{predictions.Length}");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (predictions == null)
+                {
+                    this.Close();
+                }
+                else if (predictions.Length == 0)
+                {
+                    MessageBox.Show("No Predictions");
+                    
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            bPredict.Enabled = false;
+            /* await Task.Run(() =>
+              {
+                  for (int i = 0; i <= progressBar1.Maximum; i++)
+                  {
+                      this.Invoke(new Action(() =>
+                     {
+                         UpdateProgressBar(i);
+                         this.Text = $"{i}%";
+                     }));
+                     Thread.Sleep(40);
+                  }
+              });*/
+            for (int i = 0; i <= progressBar1.Maximum; i++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    UpdateProgressBar(i);
+                    this.Text = $"{i}%";
+                }));
+                await Task.Delay(20);
+            }
+
+            int index = RandomNPredictionNumber();
+
+            string prediction = predictions[index];
+
+            MessageBox.Show($"{prediction}!");
+
+            this.Text = appName;
+            progressBar1.Value = 0;
+            bPredict.Enabled = true;
+        }
+
+        private int RandomNPredictionNumber()
+        {
+            return rand.Next(predictions.Length);
+        }
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else { progressBar1.Value = i + 1; }
+            progressBar1.Value = i;
+        }
+    }
+}
